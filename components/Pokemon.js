bbda26efd4ac00b25622a0b1a0749151937a260d
Miller Barahona <mbarahona@robinfood.com>
2021-12-12 01:30:33 -0500

    added color according to pokemon type

--- components/Pokemon.js
+++ components/Pokemon.js
@@ -1,10 +1,28 @@
 import styles from '../styles/Home.module.css';
 
 function Pokemon({ pokemon }) {
+  let className;
+
+  switch (pokemon.type) {
+    case 'fire':
+      className = styles.fire
+      break
+    case 'water':
+      className = styles.water
+      break
+    case 'grass':
+      className = styles.grass
+      break;
+    case 'poison':
+      className = styles.poison
+      break;
+    default:
+      break;
+  }
   return (
-    <div key={pokemon.name} className={styles.pokemon}>
-      <h1 className={styles.pokemon_name}>{pokemon.name}</h1>
-      <img src={pokemon.photo_url} alt="" style={{ width: '150px' }} />
+    <div key={pokemon.name} className={`${styles.pokemon} ${className}`} >
+      <h1 className={styles.pokemon_name} >{pokemon.name}</h1>
+      <img  src={pokemon.photo_url} alt="" style={{ width: '150px' }} />
     </div>
   )
 }
